0101106beebd531a4a132e0632c8ae939bb86248
krishmajumdar12 <kmajumda@purdue.edu>
2025-03-10 19:32:10 -0400

    Calibrate button

--- client/biomech/src/App.js
+++ client/biomech/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [message, setMessage] = useState('');
+
+  const handleCalibrate = () => {
+    setMessage('Calibration started...');
+    setTimeout(() => {
+      setMessage('Calibration complete!');
+    }, 2000);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <button onClick={handleCalibrate} className="calibrate-button">
+          Calibrate
+        </button>
+        {message && <p>{message}</p>}
     </div>
   );
 }
